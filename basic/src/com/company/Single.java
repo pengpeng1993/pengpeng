ffa9a9c38000d7ad9a41621b42d55d807174bfcc
pengpeng <1185788732@qq.com>
2020-07-24 19:32:20 +0800

    单例模式

---
+++ basic/src/com/company/Single.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Single {
+    public static void main(String args[]) {
+        SingleDemo s = SingleDemo.getInstance();
+        SingleDemo2 s2 = SingleDemo2.getInstance();
+    }
+}
+
+/**
+ * 1私有化构造函数
+ * 2提供公有化获取实例的方法
+ * */
+class SingleDemo {
+
+    private  SingleDemo(){}
+    private static SingleDemo s = new SingleDemo();
+    public static SingleDemo getInstance() {
+        return s;
+    }
+}
+/**
+ * todo 安全问题
+ * */
+class SingleDemo2 {
+    private  SingleDemo2(){}
+    private static SingleDemo2 s = null;
+    public static SingleDemo2 getInstance() {
+        if(s == null) {
+            s = new SingleDemo2();
+        }
+        return s;
+    }
+}
